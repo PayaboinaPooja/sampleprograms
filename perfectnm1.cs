6c3cd28dd9504e91ac8262aa3053e7d9b0d414cd
PayaboinaPooja <62057830+PayaboinaPooja@users.noreply.github.com>
2021-02-05 19:11:57 +0530

    Add files via upload

---
+++ perfectnm1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class perfectnm1
+    {
+        static void Main(string[] args)
+        {
+            int num = 28;
+            int copy = num;
+            int startnum = 1;
+          
+            int sum = 0;
+          while( startnum<num) {
+                if (num % startnum == 0)
+                {
+                    sum = sum + startnum;
+                  
+
+                }
+                startnum++;
+              
+            }
+
+            if (sum == copy)
+            {
+                Console.WriteLine("perfect");
+            }
+            else
+            {
+                Console.WriteLine("not perfect");
+            }
+
+        }
+    }
+}
